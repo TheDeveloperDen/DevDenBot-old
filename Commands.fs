86c331274d0b86d03b9e610bd3091c8acdc57ad0
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-21 21:28:58 +0000

    Doesn't work

--- Commands.fs
+++ Commands.fs
@@ -1,9 +1,23 @@
 module DevDenBot.Commands
+
 open DSharpPlus
 
 let prefix = "!"
 open FSharp.Control.Tasks
 let (>>=) m f = Option.bind f m
-let handleCommand (client: DiscordClient) (event: EventArgs.MessageCreateEventArgs) = task {
-    (Option.ofNullable event.Channel.Guild) >>= (fun x -> Some 3)
-}
+
+type CommandHandler = DiscordClient -> EventArgs.MessageCreateEventArgs -> Task<unit>
+
+let handleCommand commands (client: DiscordClient) (event: EventArgs.MessageCreateEventArgs) =
+    task {
+        if event.Channel.Guild <> null then
+            let command = event.Message.Content
+            let parts = command.Split " "
+
+            if parts.[0].StartsWith prefix then
+                let commandName = parts.[0].Substring(prefix.Length)
+                let matchingCommand = Map.tryFind commandName commands
+                return Option.map (fun x -> x()) matchingCommand
+    }
+
+let createCommandHandler commands = handleCommand commands
