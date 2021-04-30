66cc746dca1259cad00c72caeef8f1876b9bf332
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-30 13:02:19 +0000

    Fix rolelist embed maybe

--- src/main/kotlin/me/bristermitten/devdenbot/commands/roles/RoleListCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/roles/RoleListCommand.kt
@@ -3,13 +3,14 @@ package me.bristermitten.devdenbot.commands.roles
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.bristermitten.devdenbot.commands.DevDenCommand
 import me.bristermitten.devdenbot.extensions.await
+import me.bristermitten.devdenbot.serialization.DDBConfig
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.MessageBuilder
 import javax.inject.Inject
 
 class RoleListCommand @Inject constructor(
     val jda: JDA,
+    val config: DDBConfig
 ) : DevDenCommand(
     name = "roles",
     help = "Show all available roles",
@@ -17,21 +18,17 @@ class RoleListCommand @Inject constructor(
     aliases = arrayOf("rolelist")
 ) {
 
-
     override suspend fun CommandEvent.execute() {
         val roleList = ROLES
             .mapNotNull { jda.getRoleById(it) }
             .joinToString("\n") { ">> " + it.name }
-        val builder = MessageBuilder()
-            .append("""```
-                $roleList
-                ```""")
-            .setEmbed(EmbedBuilder()
-                .setTitle("Role Menu")
-                .build()
-            )
+        val builder = EmbedBuilder()
+            .setTitle("Role Menu")
+            .setDescription("```$roleList```")
+            .setColor(config.colour)
+            .build()
 
-        channel.sendMessage(builder.build()).await()
+        channel.sendMessage(builder).await()
     }
 
 }
