e619c699ad3a93375dbb5425bf711054c110b3ee
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-30 14:05:06 +0000

    Type

--- src/main/kotlin/me/bristermitten/devdenbot/commands/management/SetVarCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/management/SetVarCommand.kt
@@ -10,9 +10,10 @@ import me.bristermitten.devdenbot.extensions.commands.firstMentionedUser
 import me.bristermitten.devdenbot.serialization.DDBConfig
 import java.math.BigInteger
 import javax.inject.Inject
-import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty1
 import kotlin.reflect.KProperty1
+import kotlin.reflect.KType
+import kotlin.reflect.full.createType
 import kotlin.reflect.full.memberProperties
 
 
@@ -20,7 +21,7 @@ import kotlin.reflect.full.memberProperties
  * @author AlexL
  */
 class SetVarCommand @Inject constructor(
-    val config: DDBConfig
+    val config: DDBConfig,
 ) : DevDenCommand(
     name = "set",
     help = "Set the data of a user",
@@ -60,18 +61,18 @@ class SetVarCommand @Inject constructor(
             return
         }
 
-        val value = amount.content.parseTo(stat.returnType as KClass<*>)
+        val value = amount.content.parseTo(stat.returnType)
 
         val statsUser = StatsUsers[targetUser.idLong]
         stat.set(statsUser, value)
         awaitReply("Successfully set value of `${stat.name}` for ${targetUser.asMention} to `$value`")
     }
 
-    private fun String.parseTo(type: KClass<*>) = when(type) {
-        String::class -> this
-        Int::class -> toInt()
-        Long::class -> toLong()
-        BigInteger::class -> toBigInteger()
+    private fun String.parseTo(type: KType) = when (type) {
+        String::class.createType() -> this
+        Int::class.createType() -> toInt()
+        Long::class.createType() -> toLong()
+        BigInteger::class.createType() -> toBigInteger()
         else -> throw IllegalArgumentException("Cannot parse $type")
     }
 }
