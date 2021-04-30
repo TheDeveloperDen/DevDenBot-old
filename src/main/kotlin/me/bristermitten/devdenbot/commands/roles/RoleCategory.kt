df7ff7dcdde48546fbd37bcb7b3e43cc0a4b67e9
Redempt <instantiable@gmail.com>
2021-04-30 03:25:01 -0400

    Added role and derole commands, fixed bug

---
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/roles/RoleCategory.kt
@@ -0,0 +1,11 @@
+package me.bristermitten.devdenbot.commands.roles
+
+import me.bristermitten.devdenbot.commands.category.CommandCategory
+import net.dv8tion.jda.api.entities.Activity
+
+class RoleCategory : CommandCategory(
+    name = "Roles",
+    description = "Self-assigning roles",
+    emoji = Activity.Emoji("\uD83D\uDD34"),
+    shortName = "role"
+)
