df4f51175845fbc937d8fe1bf1ce30c36e3da275
Lars Kecker <lars.kecker2009@gmail.com>
2021-05-01 23:23:34 +0200

    test: add additional tests

--- src/test/kotlin/me/bristermitten/devdenbot/util/LevenshteinUtilsTest.kt
+++ src/test/kotlin/me/bristermitten/devdenbot/util/LevenshteinUtilsTest.kt
@@ -7,10 +7,12 @@ import kotlin.test.assertNull
 
 internal class LevenshteinUtilsTest {
 
+    private val pronouns = listOf("(she/her)", "(he/him)", "(they/them)")
+
     @Test
     fun testGetSuggestionReturnsClosestSuggestion() {
         val input = "he/him"
-        val allowedValues = listOf("(she/her)", "(he/him)", "(they/them)")
+        val allowedValues = pronouns
         val suggestion = getSuggestion(input, allowedValues);
         assertNotNull(suggestion)
         assertEquals(suggestion, "(he/him)")
@@ -19,7 +21,23 @@ internal class LevenshteinUtilsTest {
     @Test
     fun testGetSuggestionNoneMatchReturnsNull() {
         val input = "someInput"
-        val allowedValues = listOf("(she/her)", "(he/him)", "(they/them)")
+        val allowedValues = pronouns
+        val suggestion = getSuggestion(input, allowedValues);
+        assertNull(suggestion)
+    }
+
+    @Test
+    fun testAllowedValuesEmptyReturnsNull() {
+        val input = "someInput"
+        val allowedValues = listOf<String>()
+        val suggestion = getSuggestion(input, allowedValues);
+        assertNull(suggestion)
+    }
+
+    @Test
+    fun testInputEmptyReturnsNull() {
+        val input = ""
+        val allowedValues = pronouns
         val suggestion = getSuggestion(input, allowedValues);
         assertNull(suggestion)
     }
