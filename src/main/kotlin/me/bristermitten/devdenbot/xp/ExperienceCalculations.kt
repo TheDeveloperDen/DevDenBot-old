f0ae4c366ad5c9afaa0e77fa4fbb1ebee985c05c
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-02 23:41:43 +0100

    Tweak XP Algorithm

--- src/main/kotlin/me/bristermitten/devdenbot/xp/ExperienceCalculations.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/xp/ExperienceCalculations.kt
@@ -43,7 +43,7 @@ fun xpForMessage(message: String): Double {
     val compressibility = compressibility(message)
     val wordiness = wordiness(message)
     val length = message.length.toDouble()
-    return ((1 - compressibility) * 0.7 + wordiness * 0.3) * tanh(length / 30.0) * length.pow(0.75)
+    return ((1 - compressibility) * 0.7 + wordiness * 0.2) * tanh(length / 20.0) * length.pow(0.7)
 }
 
 fun xpForLevel(level: Int): BigInteger = level.toBigDecimal()
