71d51a4e498d04b60b6f8bb487fdf8198c96f3d6
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-02 23:52:49 +0100

    Fix xptest maybe

--- src/main/kotlin/me/bristermitten/devdenbot/commands/management/XPTestCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/management/XPTestCommand.kt
@@ -4,8 +4,10 @@ import com.jagrosh.jdautilities.command.CommandEvent
 import me.bristermitten.devdenbot.commands.DevDenCommand
 import me.bristermitten.devdenbot.data.StatsUsers
 import me.bristermitten.devdenbot.extensions.await
+import me.bristermitten.devdenbot.extensions.commands.awaitReply
 import me.bristermitten.devdenbot.serialization.DDBConfig
 import me.bristermitten.devdenbot.xp.xpForMessage
+import net.dv8tion.jda.api.Permission
 import java.io.File
 import javax.inject.Inject
 
@@ -18,8 +20,13 @@ class XPTestCommand @Inject constructor(
 ) : DevDenCommand(
     name = "xptest",
     help = "Find how much XP a message would give",
+
 ) {
     override suspend fun CommandEvent.execute() {
+        if(!member.hasPermission(Permission.MESSAGE_MANAGE)) {
+            awaitReply("No permission.")
+            return
+        }
         val xp = xpForMessage(args)
         event.message.reply("This message would give **$xp** XP.").await()
     }
