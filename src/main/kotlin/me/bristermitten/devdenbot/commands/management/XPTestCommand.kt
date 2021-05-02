37e3687ad6ac3e1c5ad15207de76e8f4adf16035
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-02 23:35:08 +0100

    Add XPTest command

---
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/management/XPTestCommand.kt
@@ -0,0 +1,27 @@
+package me.bristermitten.devdenbot.commands.management
+
+import com.jagrosh.jdautilities.command.CommandEvent
+import me.bristermitten.devdenbot.commands.DevDenCommand
+import me.bristermitten.devdenbot.data.StatsUsers
+import me.bristermitten.devdenbot.serialization.DDBConfig
+import me.bristermitten.devdenbot.xp.xpForMessage
+import java.io.File
+import javax.inject.Inject
+
+
+/**
+ * @author AlexL
+ */
+class XPTestCommand @Inject constructor(
+    val config: DDBConfig
+) : DevDenCommand(
+    name = "xptest",
+    help = "Find how much XP a message would give",
+) {
+    override suspend fun CommandEvent.execute() {
+        val xp = xpForMessage(args)
+        event.message.reply("This message would give **$xp** XP.")
+    }
+
+
+}
