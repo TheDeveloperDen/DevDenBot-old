bcc19f9ac255690bea2c9d86ec64199d0d7d34df
Redempt <instantiable@gmail.com>
2021-05-03 18:37:58 -0400

    Disable bump notifications temporarily

--- src/main/kotlin/me/bristermitten/devdenbot/listener/BumpNotificationListener.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/listener/BumpNotificationListener.kt
@@ -14,34 +14,34 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import java.util.concurrent.TimeUnit
 
-class BumpNotificationListener : ListenerAdapter() {
-    companion object {
-        private const val DISBOARD_BOT_ID = 302050872383242240
-        private val BUMP_COOLDOWN = TimeUnit.HOURS.toMillis(2)
-    }
-
-    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
-        fif (event.message.contentRaw != "!d bump") {
-            return
-        }
-        GlobalScope.launch {
-            val nextMessage = event.channel.onMessage()
-                .asFlow()
-                .filter { it.author.idLong == DISBOARD_BOT_ID }
-                .map { it.message.embeds }
-                .filter { it.isNotEmpty() }
-                .filter { it.first().description.contains(":thumbsup:") ?: false } //hacky but works
-                .firstOrNull() ?: return@launch
-
-
-            StatsUsers[event.author.idL️ong].bumps++
-            delay(BUMP_COOLDOWN)
-            val bumpNotificationRole =
-                requireNotNull(event.jda.getRoleById(BUMP_NOTIFICATIONS_ROLE_ID)) { "Bump Notifications role not found" }
-            event.channel.sendMessage("${bumpNotificationRole.asMention}, the server is ready to be bumped! **!d bump**")
-                .await()
-
-
-        }
-    }
-}
+//class BumpNotificationListener : ListenerAdapter() {
+//    companion object {
+//        private const val DISBOARD_BOT_ID = 302050872383242240
+//        private val BUMP_COOLDOWN = TimeUnit.HOURS.toMillis(2)
+//    }
+//
+//    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
+//        fif (event.message.contentRaw != "!d bump") {
+//            return
+//        }
+//        GlobalScope.launch {
+//            val nextMessage = event.channel.onMessage()
+//                .asFlow()
+//                .filter { it.author.idLong == DISBOARD_BOT_ID }
+//                .map { it.message.embeds }
+//                .filter { it.isNotEmpty() }
+//                .filter { it.first().description.contains(":thumbsup:") ?: false } //hacky but works
+//                .firstOrNull() ?: return@launch
+//
+//
+//            StatsUsers[event.author.idL️ong].bumps++
+//            delay(BUMP_COOLDOWN)
+//            val bumpNotificationRole =
+//                requireNotNull(event.jda.getRoleById(BUMP_NOTIFICATIONS_ROLE_ID)) { "Bump Notifications role not found" }
+//            event.channel.sendMessage("${bumpNotificationRole.asMention}, the server is ready to be bumped! **!d bump**")
+//                .await()
+//
+//
+//        }
+//    }
+//}
