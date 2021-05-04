93b61ba151b6441f2668cd917adc52e290364be1
Redempt <instantiable@gmail.com>
2021-05-04 02:45:48 -0400

    Fix nitpicks - Make MessageCache into an object instead of a class which only contains a companion object - Change MessageCache#update to modify the message rather than creating a new instance and replacing it

--- src/main/kotlin/me/bristermitten/devdenbot/data/MessageCache.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/data/MessageCache.kt
@@ -3,35 +3,33 @@ package me.bristermitten.devdenbot.data
 import java.util.*
 import kotlin.collections.HashMap
 
-class MessageCache {
-    companion object {
+object MessageCache {
 
-        private const val MAX_CACHE_SIZE = 1000;
+    private const val MAX_CACHE_SIZE = 1000;
 
-        private val cache = HashMap<Long, CachedMessage>()
-        private val byTime = PriorityQueue<CachedMessage>(Comparator.comparingLong {it.timestamp})
+    private val cache = HashMap<Long, CachedMessage>()
+    private val byTime = PriorityQueue<CachedMessage>(Comparator.comparingLong {it.timestamp})
 
-        @Synchronized
-        fun cache(message: CachedMessage) {
-            if (!cache.containsKey(message.id)) {
-                byTime.add(message)
-            }
-            cache[message.id] = message
-            if (cache.size > MAX_CACHE_SIZE) {
-                cache.remove(byTime.poll().id)
-            }
+    @Synchronized
+    fun cache(message: CachedMessage) {
+        if (!cache.containsKey(message.id)) {
+            byTime.add(message)
         }
-
-        @Synchronized
-        fun update(id: Long, msg: String) {
-            getCached(id)?.also {
-                cache[id] = CachedMessage(it.id, it.authorId, it.timestamp, msg)
-            }
+        cache[message.id] = message
+        if (cache.size > MAX_CACHE_SIZE) {
+            cache.remove(byTime.poll().id)
         }
-
-        fun getCached(id: Long): CachedMessage? {
-            return cache[id]
-        }
-
     }
+
+    @Synchronized
+    fun update(id: Long, msg: String) {
+        getCached(id)?.also {
+            it.msg = msg
+        }
+    }
+
+    fun getCached(id: Long): CachedMessage? {
+        return cache[id]
+    }
+
 }
