f36ab3aaccb0820887b37fa4ed0968c65a7e09b5
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-08 21:54:33 +0100

    Fix bump notification listener...

--- src/main/kotlin/me/bristermitten/devdenbot/listener/BumpNotificationListener.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/listener/BumpNotificationListener.kt
@@ -1,45 +1,44 @@
 package me.bristermitten.devdenbot.listener
 
-import club.minnced.jda.reactor.onMessage
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import me.bristermitten.devdenbot.commands.roles.BUMP_NOTIFICATIONS_ROLE_ID
 import me.bristermitten.devdenbot.data.StatsUsers
 import me.bristermitten.devdenbot.extensions.await
 import me.bristermitten.devdenbot.util.inc
+import me.bristermitten.devdenbot.util.listenFlow
+import me.bristermitten.devdenbot.util.scope
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
-import net.dv8tion.jda.api.hooks.ListenerAdapter
 import java.util.concurrent.TimeUnit
 
-class BumpNotificationListener : ListenerAdapter() {
+class BumpNotificationListener : EventListener {
 
     companion object {
         private const val DISBOARD_BOT_ID = 302050872383242240
         private val BUMP_COOLDOWN = TimeUnit.HOURS.toMillis(2)
     }
 
-    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
-
+    private suspend fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
         if (event.author.idLong != DISBOARD_BOT_ID) {
             return
         }
-        if(!event.message.embeds.first().description?.contains("Check it on DISBOARD:")!!) {
+
+        if (!event.message.embeds.first().description?.contains("Check it on DISBOARD:")!!) {
             return
         }
 
         StatsUsers[event.message.mentionedUsers[0].idLong].bumps++
 
-        GlobalScope.launch {
-            delay(BUMP_COOLDOWN)
-            val bumpNotificationRole =
-                    requireNotNull(event.jda.getRoleById(BUMP_NOTIFICATIONS_ROLE_ID)) { "Bump Notifications role not found" }
-            event.channel.sendMessage("${bumpNotificationRole.asMention}, the server is ready to be bumped! **!d bump**")
-                    .await()
-        }
+        delay(BUMP_COOLDOWN)
+        val bumpNotificationRole =
+            requireNotNull(event.jda.getRoleById(BUMP_NOTIFICATIONS_ROLE_ID)) { "Bump Notifications role not found" }
+        event.channel.sendMessage("${bumpNotificationRole.asMention}, the server is ready to be bumped! **!d bump**")
+            .await()
+    }
+
+    override fun register(jda: JDA) {
+        jda.listenFlow<GuildMessageReceivedEvent>().onEach(this::onGuildMessageReceived).launchIn(scope)
     }
 }
