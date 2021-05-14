aecb84ace4e866e6bb35957ff76c85703aba11ba
Lars Kecker <lars.kecker2009@gmail.com>
2021-05-14 22:38:48 +0200

    add HasteClient

--- src/main/kotlin/me/bristermitten/devdenbot/util/VersionProvider.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/util/VersionProvider.kt
@@ -1,7 +1,8 @@
 package me.bristermitten.devdenbot.util
 
+// could be renamed to provide more info if required
 object VersionProvider {
     val version by lazy {
-        javaClass.classLoader.getResourceAsStream("version.txt")!!.reader().readText()
+        javaClass.classLoader.getResourceAsStream("version.txt")!!.reader().readText().trim() // trim to remove newline before EOF
     }
 }
