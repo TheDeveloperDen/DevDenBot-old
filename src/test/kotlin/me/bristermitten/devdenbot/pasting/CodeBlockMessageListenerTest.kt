6f5890ba3a16a19c4b4b83aa217e73e682be08f3
Lars Kecker <lars.kecker2009@gmail.com>
2021-05-19 22:18:22 +0200

    test: add CodeBlockMessageListener regex tests

--- src/test/kotlin/me/bristermitten/devdenbot/pasting/CodeBlockMessageListenerTest.kt
+++ src/test/kotlin/me/bristermitten/devdenbot/pasting/CodeBlockMessageListenerTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
 
 internal class CodeBlockMessageListenerTest {
 
-    val multipleCodeBlocks = """
+    private val multipleCodeBlocks = """
         ```java
         import java.util.Scanner;
         public class MessageTooLongException {
@@ -28,13 +28,9 @@ internal class CodeBlockMessageListenerTest {
                 System.out.prinln();
         }```
         Hey guys this is some work i had, this i what i've done so far but it needs to use try/catch and im not sure how to make it so.
-        ```
-        fun main() = println(""${'"'}
-        // some small code i made""${'"'}
-        ```
     """.trimIndent()
 
-    val multipleCodeBlocksFirstCodeBlock = """
+    private val multipleCodeBlocksFirstCodeBlock = """
         import java.util.Scanner;
         public class MessageTooLongException {
 
@@ -48,7 +44,7 @@ internal class CodeBlockMessageListenerTest {
             }
     """.trimIndent()
 
-    val multipleCodeBlocksSecondCodeBlock = """
+    private val multipleCodeBlocksSecondCodeBlock = """
         public static void main(String[] args) {
                 Scanner Msg = new Scanner(System.in);
                 int x = 1;
@@ -58,30 +54,62 @@ internal class CodeBlockMessageListenerTest {
         }
         """.trimIndent()
 
-    val smallCodeBlock = """
-         Hey guys this is some work i had, this i what i've done so far but it needs to use try/catch and im not sure how to make it so.
+    private val multipleCodeBlocksOneTooSmallSecondCodeBlock = """
+        this is
+        once again
+        a smol
+        code block
+    """.trimIndent()
+
+    private val codeBlockWithLanguage = """
+        ```somelang
+        this is
+        once again
+        a smol
+        code block
         ```
-        fun main() = println(""${'"'}
-        // some small code i made""${'"'}
-        ```
+    """.trimIndent()
+
+    private val codeBlockWithoutNewLines = """
+        ```this is
+        once again
+        a smol
+        code block```
     """.trimIndent()
 
     @Test
     fun `regex matches multiple code blocks`() {
-        val matches = CodeBlockMessageListener.largeCodeBlock.findAll(multipleCodeBlocks)
+        val matches = CodeBlockMessageListener.codeBlock.findAll(multipleCodeBlocks)
         assertEquals(2, matches.count())
-        val firstMatch = matches.elementAt(0)
-        assertEquals(2, firstMatch.groups.size) // one group is the match, the other is the code block
-        assertEquals(multipleCodeBlocksFirstCodeBlock, firstMatch.groups[1]!!.value.trimEnd()) // newLines at the end are fine
+        val firstMatchGroups = matches.elementAt(0).groups.filterNotNull()
+        // first group is the whole match, 2nd group the language if given and 3rd group is the code block
+        assertEquals(3, firstMatchGroups.size)
+        assertEquals(multipleCodeBlocksFirstCodeBlock, firstMatchGroups.last()!!.value.trimEnd())
+        assertEquals("java", firstMatchGroups[1]!!.value)
 
-        val secondMatch = matches.elementAt(1)
-        assertEquals(2, secondMatch.groups.size)
-        assertEquals(multipleCodeBlocksSecondCodeBlock, secondMatch.groups[1]!!.value.trimEnd())
+        val secondMatchGroups = matches.elementAt(1).groups.filterNotNull()
+        assertEquals(2,  secondMatchGroups.size)
+        assertEquals(multipleCodeBlocksSecondCodeBlock, secondMatchGroups.last().value.trimEnd())
     }
 
     @Test
-    fun `regex does not match small code block`() {
-        val matches = CodeBlockMessageListener.largeCodeBlock.findAll(smallCodeBlock)
-        assertEquals(0, matches.count())
+    fun `regex matches code block with language`() {
+        val matches = CodeBlockMessageListener.codeBlock.findAll(codeBlockWithLanguage)
+        assertEquals(1, matches.count())
+        val firstMatchGroups = matches.elementAt(0).groups.filterNotNull()
+        assertEquals(3, firstMatchGroups.size)
+        assertEquals(multipleCodeBlocksOneTooSmallSecondCodeBlock, firstMatchGroups.last()!!.value.trimEnd())
+        assertEquals("somelang", firstMatchGroups[1]!!.value)
+
+    }
+
+    @Test
+    fun `regex matches code block without newlines`() {
+        val matches = CodeBlockMessageListener.codeBlock.findAll(codeBlockWithoutNewLines)
+        assertEquals(1, matches.count())
+        val firstMatchGroups = matches.elementAt(0).groups.filterNotNull()
+        assertEquals(2, firstMatchGroups.size)
+        assertEquals(multipleCodeBlocksOneTooSmallSecondCodeBlock, firstMatchGroups.last()!!.value.trimEnd())
+
     }
 }
