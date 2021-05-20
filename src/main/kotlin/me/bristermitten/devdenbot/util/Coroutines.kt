5a355bf8e3e77860cdbd8a494ac252ff0460f780
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-20 23:31:39 +0100

    Fix sentry catching in listeners maybe yes im pushing to master i dont care

--- src/main/kotlin/me/bristermitten/devdenbot/util/Coroutines.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/util/Coroutines.kt
@@ -5,7 +5,6 @@ import io.sentry.Sentry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
@@ -16,18 +15,15 @@ val scope = CoroutineScope(Dispatchers.Default)
 
 inline fun <reified T : GenericEvent> JDA.listenFlow() = on<T>().asFlow()
 
-fun <T> Flow<T>.catchWithSentry() = catch {
-    Sentry.captureException(it)
-}
 
-inline fun <T> Flow<T>.handleEachIn(scope: CoroutineScope, crossinline run: suspend (T) -> Unit) =
-    catchWithSentry()
-        .launchEachIn(scope, run)
-
-inline fun <T> Flow<T>.launchEachIn(scope: CoroutineScope, crossinline run: suspend (T) -> Unit) = scope.launch {
+inline fun <T> Flow<T>.handleEachIn(scope: CoroutineScope, crossinline run: suspend (T) -> Unit) = scope.launch {
     collect {
         scope.launch {
-            run(it)
+            try {
+                run(it)
+            } catch (e: Exception) {
+                Sentry.captureException(e)
+            }
         }
     }
 }
