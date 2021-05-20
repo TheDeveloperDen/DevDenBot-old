2cd6817216e0fe2974193deb6c07638ab12f6cba
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-20 20:38:28 +0100

    Add the leave and join events

--- src/main/kotlin/me/bristermitten/devdenbot/events/EventsListener.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/events/EventsListener.kt
@@ -6,9 +6,12 @@ import me.bristermitten.devdenbot.util.handleEachIn
 import me.bristermitten.devdenbot.util.listenFlow
 import me.bristermitten.devdenbot.util.scope
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import java.time.LocalDateTime
 
 @Used
 class EventsListener : EventListener {
@@ -18,6 +21,27 @@ class EventsListener : EventListener {
             Events.insert {
                 it[action] = EventType.USER_MESSAGE
                 it[timestamp] = event.message.timeCreated.toLocalDateTime()
+                it[data] = event.channel.idLong
+            }
+        }
+    }
+
+    private suspend fun onGuildJoin(event: GuildMemberJoinEvent) {
+        newSuspendedTransaction {
+            Events.insert {
+                it[action] = EventType.USER_JOIN
+                it[timestamp] = LocalDateTime.now()
+                it[data] = event.user.idLong
+            }
+        }
+    }
+
+    private suspend fun onGuildLeave(event: GuildMemberRemoveEvent) {
+        newSuspendedTransaction {
+            Events.insert {
+                it[action] = EventType.USER_LEAVE
+                it[timestamp] = LocalDateTime.now()
+                it[data] = event.user.idLong
             }
         }
     }
@@ -25,5 +49,11 @@ class EventsListener : EventListener {
     override fun register(jda: JDA) {
         jda.listenFlow<GuildMessageReceivedEvent>()
             .handleEachIn(scope, this::onGuildMessageReceived)
+
+        jda.listenFlow<GuildMemberJoinEvent>()
+            .handleEachIn(scope, this::onGuildJoin)
+
+        jda.listenFlow<GuildMemberRemoveEvent>()
+            .handleEachIn(scope, this::onGuildLeave)
     }
 }
