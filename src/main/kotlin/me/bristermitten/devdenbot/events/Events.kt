6ee65d7afe40b8253d59459f245f0473a5f818fc
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-20 17:56:09 +0100

    Change Events table schema

--- src/main/kotlin/me/bristermitten/devdenbot/events/Events.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/events/Events.kt
@@ -1,12 +1,12 @@
 package me.bristermitten.devdenbot.events
 
-import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.`java-time`.datetime
 
 object Events : Table() {
     val id = integer("id").autoIncrement()
     val action = enumerationByName("event_type", 32, EventType::class)
+    val data = long("data") //The ID of the event's data (channel id, user id, etc)
     val timestamp = datetime("timestamp")
     override val primaryKey = PrimaryKey(id)
 }
