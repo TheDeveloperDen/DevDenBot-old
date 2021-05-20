ff144068d6a1cbeff0ed38ac8bc727c107ef2c0c
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-20 19:41:33 +0100

    Remove redundant SQL package

--- src/main/kotlin/me/bristermitten/devdenbot/sql/SQLConfig.kt
+++
@@ -1,9 +0,0 @@
-package me.bristermitten.devdenbot.sql
-
-data class SQLConfig(
-    val hostname: String,
-    val port: Int,
-    val database: String,
-    val username: String,
-    val password: String,
-)
--- src/main/kotlin/me/bristermitten/devdenbot/sql/SQLSerializable.kt
+++
@@ -1,8 +0,0 @@
-package me.bristermitten.devdenbot.sql
-
-import java.sql.Connection
-
-interface SQLSerializable {
-    suspend fun load(connection: Connection)
-    suspend fun save(connection: Connection)
-}
