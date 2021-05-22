2d47e9e7e802b6db3fc36e5ea53c32c92a2460c7
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-22 09:54:56 +0100

    Fix the fixxpcommand (ironic)

--- src/main/kotlin/me/bristermitten/devdenbot/commands/management/FixXPCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/management/FixXPCommand.kt
@@ -3,10 +3,11 @@ package me.bristermitten.devdenbot.commands.management
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.bristermitten.devdenbot.commands.DevDenCommand
 import me.bristermitten.devdenbot.data.StatsUser
+import me.bristermitten.devdenbot.extensions.await
 import me.bristermitten.devdenbot.inject.Used
-import me.bristermitten.devdenbot.serialization.DDBConfig
 import me.bristermitten.devdenbot.xp.processLevelUp
 import me.bristermitten.devdenbot.xp.xpForLevel
+import net.dv8tion.jda.api.exceptions.ErrorResponseException
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import javax.inject.Inject
 
@@ -25,15 +26,22 @@ class FixXPCommand @Inject constructor(
     override suspend fun CommandEvent.execute() {
         newSuspendedTransaction {
             val all = StatsUser.all()
-            for (user in all) {
-                val member = guild.getMemberById(user.id.value) ?: continue // user probably left the guild
-                while (xpForLevel(user.level) <= user.xp) {
+            all.forEach { user ->
+                println(user.id)
+                val member = try {
+                    guild.retrieveMemberById(user.id.value).await()
+                } catch (e: ErrorResponseException) {
+                    println("${user.id.value} was not in guild")
+                    return@forEach // user probably left the guild
+                }
+                while (xpForLevel(user.level + 1) <= user.xp) {
                     processLevelUp(member, ++user.level)
                 }
             }
+            reply("All level ups given i think")
         }
-
     }
 
-
 }
+
+
