d50e435c0374bd08b00c1b0f916795fdba47a112
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-05-22 09:34:48 +0100

    Remove unused imports

--- src/main/kotlin/me/bristermitten/devdenbot/commands/management/FixXPCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/management/FixXPCommand.kt
@@ -3,21 +3,11 @@ package me.bristermitten.devdenbot.commands.management
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.bristermitten.devdenbot.commands.DevDenCommand
 import me.bristermitten.devdenbot.data.StatsUser
-import me.bristermitten.devdenbot.data.StatsUsers
-import me.bristermitten.devdenbot.events.EventType
-import me.bristermitten.devdenbot.events.Events
 import me.bristermitten.devdenbot.inject.Used
 import me.bristermitten.devdenbot.serialization.DDBConfig
-import me.bristermitten.devdenbot.util.VersionProvider
-import me.bristermitten.devdenbot.util.formatNumber
-import me.bristermitten.devdenbot.util.log
 import me.bristermitten.devdenbot.xp.processLevelUp
 import me.bristermitten.devdenbot.xp.xpForLevel
-import net.dv8tion.jda.api.EmbedBuilder
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 /**
@@ -25,7 +15,6 @@ import javax.inject.Inject
  */
 @Used
 class FixXPCommand @Inject constructor(
-    private val ddbConfig: DDBConfig,
 ) : DevDenCommand(
     name = "fixxp",
     help = "Fix broken XP",
@@ -36,9 +25,9 @@ class FixXPCommand @Inject constructor(
     override suspend fun CommandEvent.execute() {
         newSuspendedTransaction {
             val all = StatsUser.all()
-            for(user in all) {
+            for (user in all) {
                 val member = guild.getMemberById(user.id.value) ?: continue // user probably left the guild
-                while(xpForLevel(user.level) <= user.xp){
+                while (xpForLevel(user.level) <= user.xp) {
                     processLevelUp(member, ++user.level)
                 }
             }
