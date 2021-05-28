e6d185390f27113d7407d7c2fa867c8527d8971a
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2021-05-28 17:41:04 -0400

    Prevent self reactions

--- src/main/kotlin/me/bristermitten/devdenbot/listener/ShowcaseMessageListener.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/listener/ShowcaseMessageListener.kt
@@ -8,6 +8,7 @@ import me.bristermitten.devdenbot.util.listenFlow
 import me.bristermitten.devdenbot.util.scope
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent
 
 @Used
 class ShowcaseMessageListener : EventListener {
@@ -24,7 +25,17 @@ class ShowcaseMessageListener : EventListener {
         event.message.addReaction(MINUS_ONE).await()
     }
 
+    suspend fun onGuildMessageReactionAdd(event: GuildMessageReactionAddEvent) {
+        if (event.channel.idLong != SHOWCASE_CHANNEL_ID) {
+            return
+        }
+        if (event.user.idLong == event.retrieveMessage().await().author.idLong) {
+            event.reaction.removeReaction(event.user).await()
+        }
+    }
+
     override fun register(jda: JDA) {
         jda.listenFlow<GuildMessageReceivedEvent>().handleEachIn(scope, this::onGuildMessageReceived)
+        jda.listenFlow<GuildMessageReactionAddEvent>().handleEachIn(scope, this::onGuildMessageReactionAdd)
     }
 }
