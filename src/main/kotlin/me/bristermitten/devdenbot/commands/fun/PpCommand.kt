a7ddb187a52415e1ac2ae2f0949f967d883dc0ab
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-06-04 18:34:18 +0100

    Revert "Merge pull request #100 from BlehKong/master"
    This reverts commit 8b0cd61ee1897672b37cd652515e835b1b96d3fc.

--- src/main/kotlin/me/bristermitten/devdenbot/commands/fun/PpCommand.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/commands/fun/PpCommand.kt
@@ -2,9 +2,7 @@ package me.bristermitten.devdenbot.commands.info
 
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.bristermitten.devdenbot.commands.DevDenCommand
-import me.bristermitten.devdenbot.commands.arguments.arguments
 import me.bristermitten.devdenbot.extensions.await
-import me.bristermitten.devdenbot.extensions.commands.awaitReply
 import me.bristermitten.devdenbot.extensions.commands.firstMentionedUser
 import me.bristermitten.devdenbot.inject.Used
 import net.dv8tion.jda.api.EmbedBuilder
@@ -26,7 +24,11 @@ class PpCommand @Inject constructor(
 
     override suspend fun CommandEvent.execute() {
 
-        val user = firstMentionedUser() ?: event.message.author
+        if(args[0].equals(null)) { message.channel.sendMessage("Please specify args.").await(); return }
+
+        val user = firstMentionedUser() ?: jda.getUserById(args[0].toString())
+
+        if(user == null) { message.channel.sendMessage("Unable to find that user.").await(); return }
 
         val length = StringUtils.repeat('=', (0..16).random())
 
