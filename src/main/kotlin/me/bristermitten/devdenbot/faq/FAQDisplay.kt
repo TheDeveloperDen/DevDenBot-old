2b70b194039bfaae29a0407f76977c0133eb5130
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-06-10 07:37:24 +0100

    Maybe fix things

--- src/main/kotlin/me/bristermitten/devdenbot/faq/FAQDisplay.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/faq/FAQDisplay.kt
@@ -7,16 +7,22 @@ import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.User
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
-suspend fun HasConfig.displayFAQ(name: String, user: User): MessageEmbed {
-    val faq = newSuspendedTransaction {
-        FAQDAO.find {
-            FAQs.name eq name
-        }.firstOrNull()?.toPOJO()
-    } ?: return embedDefaults {
+private suspend fun HasConfig.unknownFAQ(name: String, user: User) = embedDefaults {
         title = "Unknown FAQ"
         description = "Unknown FAQ `$name`"
         setFooter("Requested by ${user.name}#${user.discriminator} | $name", user.avatarUrl)
     }
+
+suspend fun HasConfig.displayFAQ(name: String, user: User): MessageEmbed {
+    if (name.length > 32) {
+        return unknownFAQ(name, user)
+    }
+
+    val faq = newSuspendedTransaction {
+        FAQDAO.find {
+            FAQs.name eq name
+        }.firstOrNull()?.toPOJO()
+    } ?: return unknownFAQ(name, user) 
 
     return embedDefaults {
         author = "FAQ Answer"
