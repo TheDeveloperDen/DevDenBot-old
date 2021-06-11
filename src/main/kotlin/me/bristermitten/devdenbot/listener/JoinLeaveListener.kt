d5dbed5cbf063008d466120fd8d4e87ac71598b2
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-06-11 20:12:51 +0100

    Add leave message to make people feel bad for leaving

--- src/main/kotlin/me/bristermitten/devdenbot/listener/JoinLeaveListener.kt
+++ src/main/kotlin/me/bristermitten/devdenbot/listener/JoinLeaveListener.kt
@@ -8,17 +8,25 @@ import me.bristermitten.devdenbot.util.scope
 import me.bristermitten.devdenbot.discord.WELCOME_CHANNEL_ID
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent
 
 
 @Used
-class JoinListener : EventListener {
+class JoinLeaveListener : EventListener {
     private suspend fun onGuildMemberJoin(event: GuildMemberJoinEvent) {
         val channel = event.jda.getTextChannelById(WELCOME_CHANNEL_ID)
         channel?.sendMessage("Welcome ${event.user.asMention} to the Developer's Den! There are now ${event.guild.memberCount} users.")
             ?.await()
     }
 
+    private suspend fun onGuildMemberLeave(event: GuildMemberRemoveEvent) {
+        val channel = event.jda.getTextChannelById(WELCOME_CHANNEL_ID)
+        channel?.sendMessage("${event.user.asMention} has left \\:(. There are now ${event.guild.memberCount} users.")
+            ?.await()
+    }
+
     override fun register(jda: JDA) {
         jda.listenFlow<GuildMemberJoinEvent>().handleEachIn(scope, this::onGuildMemberJoin)
+        jda.listenFlow<GuildMemberRemoveEvent>().handleEachIn(scope, this::onGuildMemberLeave)
     }
 }
