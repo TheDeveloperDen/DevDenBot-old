fc4096d48948e93a5f2e3b2a7f77f5a2e178f80f
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-08-03 14:16:26 +0100

    Capital letters please ;-;

--- src/main/kotlin/net/developerden/devdenbot/DevDen.kt
+++ src/main/kotlin/net/developerden/devdenbot/DevDen.kt
@@ -99,7 +99,7 @@ class DevDen {
     }
 
     private fun startTasks(jda: JDA) {
-        log.info { "starting tasks..." }
+        log.info { "Starting tasks..." }
         val timer = Timer()
         timer.schedule(VoiceChatXPTask(jda), 0L, TimeUnit.SECONDS.toMillis(60))
     }
