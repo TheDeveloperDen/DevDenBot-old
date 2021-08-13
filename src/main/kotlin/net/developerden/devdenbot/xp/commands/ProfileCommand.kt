6eb520ca6d428898407a8b8d94c1322316a6e665
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-13 21:15:43 +0100

    migrate profile command to a slash command

--- src/main/kotlin/net/developerden/devdenbot/xp/commands/ProfileCommand.kt
+++ src/main/kotlin/net/developerden/devdenbot/xp/commands/ProfileCommand.kt
@@ -2,13 +2,20 @@ package net.developerden.devdenbot.xp.commands
 
 import com.jagrosh.jdautilities.command.CommandEvent
 import net.developerden.devdenbot.commands.DevDenCommand
+import net.developerden.devdenbot.commands.slash.DevDenSlashCommand
 import net.developerden.devdenbot.data.StatsUsers
 import net.developerden.devdenbot.extensions.await
+import net.developerden.devdenbot.extensions.commands.embedDefaults
 import net.developerden.devdenbot.extensions.commands.getUser
 import net.developerden.devdenbot.extensions.commands.prepareReply
 import net.developerden.devdenbot.inject.Used
+import net.developerden.devdenbot.serialization.DDBConfig
+import net.developerden.devdenbot.trait.HasConfig
 import net.developerden.devdenbot.util.formatNumber
 import net.developerden.devdenbot.xp.xpForLevel
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.requests.restaction.CommandCreateAction
 import javax.inject.Inject
 import kotlin.coroutines.resume
 
@@ -18,36 +25,29 @@ import kotlin.coroutines.resume
  */
 @Used
 class ProfileCommand @Inject constructor(
-) : DevDenCommand(
+    override val ddbConfig: DDBConfig
+) : DevDenSlashCommand(
     name = "profile",
-    help = "View your profile",
-    category = XPCategory,
-) {
+    description = "View your or someone else's profile"
+), HasConfig {
 
+    override fun load(action: CommandCreateAction) {
+        action.addOption(OptionType.USER, "target", "User to get the profile of", false)
+    }
 
+    override suspend fun SlashCommandEvent.execute() {
+        val targetUser = getOption("target")?.asUser ?: user
 
-    override suspend fun CommandEvent.execute() {
-        val args = event.message.contentRaw.removePrefix(client.prefix)
-            .dropWhile { !it.isWhitespace() }.trimStart()
+        val statsUser = StatsUsers.get(targetUser.idLong)
 
-        // what is this monstrosity
-        val targetUser = getUser()
-
-        if (targetUser == null) {
-            channel.sendMessage("Unknown user: $args").queue().also { return }
-        }
-
-        val statsUser = StatsUsers.get(targetUser!!.idLong)
-
-        val action = prepareReply {
+        val action = embedDefaults {
             title = "Your Statistics"
             field("XP", formatNumber(statsUser.xp), true)
             field("Level", statsUser.level.toString(), true)
             field("Disboard Bumps", statsUser.bumps.toString(), true)
             field("XP to Level", formatNumber(xpForLevel(statsUser.level + 1)), true)
-            setFooter("Statistics for ${targetUser.name}")
+            setFooter("Statistics for ${targetUser.name}#${targetUser.discriminator}")
         }
-
-        action.await()
+        replyEmbeds(action).await()
     }
 }
