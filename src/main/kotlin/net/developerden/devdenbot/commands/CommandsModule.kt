d5cd1e47e734a0e384bd1e7dbe6f3a1a49f85a86
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-13 16:15:23 +0100

    Make slash commands work!

--- src/main/kotlin/net/developerden/devdenbot/commands/CommandsModule.kt
+++ src/main/kotlin/net/developerden/devdenbot/commands/CommandsModule.kt
@@ -5,8 +5,10 @@ import com.jagrosh.jdautilities.command.CommandClient
 import dev.misfitlabs.kotlinguice4.KotlinModule
 import dev.misfitlabs.kotlinguice4.multibindings.KotlinMultibinder
 import dev.misfitlabs.kotlinguice4.multibindings.KotlinMultibindingsScanner
+import net.developerden.devdenbot.commands.slash.DevDenSlashCommand
 import net.developerden.devdenbot.inject.Classpath
 import java.lang.reflect.Modifier
+import kotlin.reflect.full.isSuperclassOf
 
 /**
  * @author Alexander Wood (BristerMitten)
@@ -16,13 +18,18 @@ class CommandsModule : KotlinModule() {
 
     override fun configure() {
         val commands = Classpath.subtypesOf(DevDenCommand::class.java)
-        val binder = KotlinMultibinder.newSetBinder<DevDenCommand>(kotlinBinder)
+        val normalCommandBinder = KotlinMultibinder.newSetBinder<DevDenCommand>(kotlinBinder)
+        val slashCommandBinder = KotlinMultibinder.newSetBinder<DevDenSlashCommand>(kotlinBinder)
 
         for (command in commands) {
             if (Modifier.isAbstract(command.modifiers)) {
                 continue
             }
-            binder.addBinding().to(command)
+            normalCommandBinder.addBinding().to(command)
+            if (DevDenSlashCommand::class.isSuperclassOf(command.kotlin)) {
+                @Suppress("UNCHECKED_CAST")
+                slashCommandBinder.addBinding().to(command as Class<out DevDenSlashCommand>)
+            }
         }
 
         install(KotlinMultibindingsScanner.asModule())
--- src/main/kotlin/net/developerden/devdenbot/commands/slash/DevDenSlashCommand.kt
+++ src/main/kotlin/net/developerden/devdenbot/commands/slash/DevDenSlashCommand.kt
@@ -1,9 +1,17 @@
 package net.developerden.devdenbot.commands.slash
 
+import com.jagrosh.jdautilities.command.CommandEvent
 import net.developerden.devdenbot.commands.DevDenCommand
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.requests.restaction.CommandCreateAction
 
 abstract class DevDenSlashCommand(name: String, description: String) : DevDenCommand(name, help = description) {
 
     abstract fun load(action: CommandCreateAction)
+
+    final override suspend fun CommandEvent.execute() {
+        reply("This command must be used via a slash command")
+    }
+
+    abstract suspend fun SlashCommandEvent.execute()
 }
