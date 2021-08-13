129a000884daf5b6b13e63219fd758d0b3632c4f
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-13 17:06:42 +0100

    update InfoCommand.kt to use a slash command

--- src/main/kotlin/net/developerden/devdenbot/commands/info/InfoCommand.kt
+++ src/main/kotlin/net/developerden/devdenbot/commands/info/InfoCommand.kt
@@ -1,15 +1,17 @@
 package net.developerden.devdenbot.commands.info
 
-import com.jagrosh.jdautilities.command.CommandEvent
-import net.developerden.devdenbot.commands.DevDenCommand
-import net.developerden.devdenbot.commands.management.ManagingCategory
+import net.developerden.devdenbot.commands.slash.DevDenSlashCommand
 import net.developerden.devdenbot.data.StatsUsers
 import net.developerden.devdenbot.events.Messages
+import net.developerden.devdenbot.extensions.await
+import net.developerden.devdenbot.extensions.commands.embedDefaults
 import net.developerden.devdenbot.inject.Used
 import net.developerden.devdenbot.serialization.DDBConfig
+import net.developerden.devdenbot.trait.HasConfig
 import net.developerden.devdenbot.util.VersionProvider
 import net.developerden.devdenbot.util.formatNumber
-import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.requests.restaction.CommandCreateAction
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.time.LocalDate
@@ -21,40 +23,42 @@ import javax.inject.Inject
  */
 @Used
 class InfoCommand @Inject constructor(
-    private val ddbConfig: DDBConfig,
-) : DevDenCommand(
+    override val ddbConfig: DDBConfig,
+) : DevDenSlashCommand(
     name = "info",
-    help = "View bot info and some cool stats",
-    category = InfoCategory,
-    aliases = arrayOf("flex", "stats")
-) {
+    description = "View bot info and some cool stats",
+), HasConfig {
 
     private fun formatDate(dt: LocalDate) = DateTimeFormatter.ofPattern("YYYY MM dd").format(dt)
+    override fun load(action: CommandCreateAction) = Unit
 
-    override suspend fun CommandEvent.execute() {
+
+    override suspend fun SlashCommandEvent.execute() {
         fun formatForInfo(s: String) = "`$s`"
         fun formatForInfo(s: Number) = "`${formatNumber(s)}`"
 
         val all = StatsUsers.all()
         val totalXP = formatForInfo(all.sumOf { it.xp })
-        val totalMembers = formatForInfo(event.guild.memberCount)
-        val dateCreated = formatForInfo(formatDate(event.guild.timeCreated.toLocalDate()))
+        val currentMembers = formatForInfo(guild!!.memberCount)
+        val totalMembersStored = formatForInfo(all.size)
+        val dateCreated = formatForInfo(formatDate(guild!!.timeCreated.toLocalDate()))
         val levelUps = formatForInfo(all.sumOf { it.level })
         val formattedVersion = formatForInfo(VersionProvider.version)
         val messageCount = formatForInfo(newSuspendedTransaction {
             Messages.select { Messages.type eq Messages.Type.CREATE }.count()
         })
-        reply(EmbedBuilder()
-            .setTitle("Developer Den")
-            .setDescription("Mildly interesting stats and info")
-            .setColor(ddbConfig.colour)
-            .addField("Version", formattedVersion, true)
-            .addField("Total XP Given", totalXP, true)
-            .addField("Total Members", totalMembers, true)
-            .addField("Date Created", dateCreated, true)
-            .addField("Total Messages Sent", messageCount, true)
-            .addField("Level Ups", levelUps, true)
-            .build())
+
+        replyEmbeds(embedDefaults {
+            title = "Developer Den"
+            description = "Mildly interesting stats and info"
+            addField("Version", formattedVersion, true)
+            addField("Total XP Given", totalXP, true)
+            addField("Current Member Count", currentMembers, true)
+            addField("Total Members Stored", totalMembersStored, true)
+            addField("Date Created", dateCreated, true)
+            addField("Total Messages Sent", messageCount, true)
+            addField("Level Ups", levelUps, true)
+        }).await()
     }
 
 
